a3e99c199d48ee5893b08d8fc16d16e8a159710b
jeckbjy <jeckbjy@163.com>
2016-03-26 11:22:38 +0800

    graphics

--- plugins/render_d3d12/D3D12_Pipeline.cpp
+++ plugins/render_d3d12/D3D12_Pipeline.cpp
@@ -3,33 +3,56 @@
 
 CU_NS_BEGIN
 
-D3D12Pipeline::D3D12Pipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc)
-	: m_pipeline(NULL)
+D3D12GraphicsPipeline::D3D12GraphicsPipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc)
 {
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC pso;
-	ZeroMemory(&pso, sizeof(pso));
-
-	D3D12_ROOT_SIGNATURE_DESC sign_desc;
-	//convert
-	pso.PrimitiveTopologyType = D3D12Mapping::getTopologyType(desc.topology);
-	D3D12Mapping::fillRasterizerState(pso.RasterizerState, desc.rasterizer);
-	D3D12Mapping::fillDepthStencilState(pso.DepthStencilState, desc.depthStencil);
-
-	D3D12_CHECK(device->CreateGraphicsPipelineState(&pso, IID_PPV_ARGS(&m_pipeline)), "CreateGraphicsPipelineState fail!");
+	//
 }
 
-D3D12Pipeline::D3D12Pipeline(ID3D12Device* device, const ComputePipelineDesc& desc)
-	: m_pipeline(NULL)
+D3D12GraphicsPipeline::~D3D12GraphicsPipeline()
 {
-	D3D12_COMPUTE_PIPELINE_STATE_DESC pso;
-	ZeroMemory(&pso, sizeof(pso));
 
-	D3D12_CHECK(device->CreateComputePipelineState(&pso, IID_PPV_ARGS(&m_pipeline)), "CreateComputePipelineState fail!");
 }
 
-D3D12Pipeline::~D3D12Pipeline()
+D3D12ComputePipeline::D3D12ComputePipeline(ID3D12Device* device, const ComputePipelineDesc& desc)
+{
+	D3D12_COMPUTE_PIPELINE_STATE_DESC pso = {};
+	pso.pRootSignature = NULL;
+	//pso.CS.
+	device->CreateComputePipelineState(&pso, IID_PPV_ARGS(&m_pipeline));
+}
+
+D3D12ComputePipeline::~D3D12ComputePipeline()
 {
 	D3D12_RELEASE(m_pipeline);
 }
+
+//D3D12Pipeline::D3D12Pipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc)
+//	: m_pipeline(NULL)
+//{
+//	D3D12_GRAPHICS_PIPELINE_STATE_DESC pso;
+//	ZeroMemory(&pso, sizeof(pso));
+//
+//	D3D12_ROOT_SIGNATURE_DESC sign_desc;
+//	//convert
+//	pso.PrimitiveTopologyType = D3D12Mapping::getTopologyType(desc.topology);
+//	D3D12Mapping::fillRasterizerState(pso.RasterizerState, desc.rasterizer);
+//	D3D12Mapping::fillDepthStencilState(pso.DepthStencilState, desc.depthStencil);
+//
+//	D3D12_CHECK(device->CreateGraphicsPipelineState(&pso, IID_PPV_ARGS(&m_pipeline)), "CreateGraphicsPipelineState fail!");
+//}
+//
+//D3D12Pipeline::D3D12Pipeline(ID3D12Device* device, const ComputePipelineDesc& desc)
+//	: m_pipeline(NULL)
+//{
+//	D3D12_COMPUTE_PIPELINE_STATE_DESC pso;
+//	ZeroMemory(&pso, sizeof(pso));
+//
+//	D3D12_CHECK(device->CreateComputePipelineState(&pso, IID_PPV_ARGS(&m_pipeline)), "CreateComputePipelineState fail!");
+//}
+//
+//D3D12Pipeline::~D3D12Pipeline()
+//{
+//	D3D12_RELEASE(m_pipeline);
+//}
 
 CU_NS_END
--- plugins/render_d3d12/D3D12_Pipeline.h
+++ plugins/render_d3d12/D3D12_Pipeline.h
@@ -3,34 +3,57 @@
 
 CU_NS_BEGIN
 
+// InputLayout和RenderPass如何处理呢？？
+class CU_D3D12_API D3D12GraphicsPipeline : public Pipeline
+{
+public:
+	D3D12GraphicsPipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc);
+	~D3D12GraphicsPipeline();
+
+private:
+	typedef std::map<uint32_t, ID3D12PipelineState*> PipelineMap;
+	PipelineMap m_pipelines;
+	GraphicsPipelineDesc m_desc;
+};
+
+class CU_D3D12_API D3D12ComputePipeline : public Pipeline
+{
+public:
+	D3D12ComputePipeline(ID3D12Device* device, const ComputePipelineDesc& desc);
+	~D3D12ComputePipeline();
+
+private:
+	ID3D12PipelineState* m_pipeline;
+};
+
 // 
 /*
 问题：D3D12_GRAPHICS_PIPELINE_STATE_DESC的初始化
 1:需要InputLayout，而这些数据的获得会延迟得到，与shader无关
 2:D3D12_ROOT_SIGNATURE_DESC初始化：跟shader相关，应该可以通过Shader自动解析得到,类似PipelineLayout
 */
-class CU_D3D12_API D3D12GraphicsPipeline : public Pipeline
-{
-public:
-
-	//void bind(InputLayout* layout);
-
-private:
-	// 通过VertexLayout查找
-	typedef std::map<uint32_t, ID3D12PipelineState*> PSOMap;
-	// 需要持有多个？？通过Vertex查找
-	//ID3D12PipelineState* m_pso;
-};
-
-class CU_D3D12_API D3D12Pipeline : public Pipeline
-{
-public:
-	D3D12Pipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc);
-	D3D12Pipeline(ID3D12Device* device, const ComputePipelineDesc& desc);
-	~D3D12Pipeline();
-
-private:
-	ID3D12PipelineState* m_pipeline;
-};
+//class CU_D3D12_API D3D12GraphicsPipeline : public Pipeline
+//{
+//public:
+//
+//	//void bind(InputLayout* layout);
+//
+//private:
+//	// 通过VertexLayout查找
+//	typedef std::map<uint32_t, ID3D12PipelineState*> PSOMap;
+//	// 需要持有多个？？通过Vertex查找
+//	//ID3D12PipelineState* m_pso;
+//};
+//
+//class CU_D3D12_API D3D12Pipeline : public Pipeline
+//{
+//public:
+//	D3D12Pipeline(ID3D12Device* device, const GraphicsPipelineDesc& desc);
+//	D3D12Pipeline(ID3D12Device* device, const ComputePipelineDesc& desc);
+//	~D3D12Pipeline();
+//
+//private:
+//	ID3D12PipelineState* m_pipeline;
+//};
 
 CU_NS_END
